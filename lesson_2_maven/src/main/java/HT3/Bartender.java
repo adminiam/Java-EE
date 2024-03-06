53851acaa36a1646cade11dbe037828dec655ebb
vladimirtodoshchak <vladimirtodoshchak@gmail.com>
2024-03-06 20:34:13 +0200

    HT2 improved

---
+++ lesson_2_maven/src/main/java/HT3/Bartender.java
@@ -0,0 +1,22 @@
+package HT3;
+
+public class Bartender implements Runnable {
+    private String name;
+    private Bar bar;
+
+    public Bartender(String name, Bar bar) {
+        this.name = name;
+        this.bar = bar;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (true) {
+                bar.receiveOrder(name, bar.getOrders().take(), bar);
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
